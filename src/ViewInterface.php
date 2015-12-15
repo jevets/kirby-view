3c719030b51b59fc393f8838d167dcfa4629a2ee
stevejamesson <steve@evendev.com>
2015-12-15 07:28:44 -0500

    progressing

---
+++ src/ViewInterface.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace KirbyCasts\Kirby;
+
+interface ViewInterface
+{
+    /**
+     * Create an instance of the view
+     *
+     * @param string $viewsDir
+     * @param string $cacheDir
+     * @return void
+     */
+    public function __construct($viewsDir, $cacheDir = '');
+
+    /**
+     * Get a rendered view as a string
+     *
+     * @param string $view filename of view
+     * @param array $data Data to pass to the view
+     * @return string
+     */
+    public function make($view, $data = []);
+}
