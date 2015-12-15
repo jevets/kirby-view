c65d7aa2d2b07f9d5dfa669d4067800f2760b478
stevejamesson <steve@evendev.com>
2015-12-15 10:02:17 -0500

    file extension

--- src/ViewInterface.php
+++ src/ViewInterface.php
@@ -14,6 +14,14 @@ interface ViewInterface
     public function __construct($viewsDir, $cacheDir = '');
 
     /**
+     * Set the file extension for the View engine
+     *
+     * @param string $extension
+     * @return void
+     */
+    public static function setFileExtension($extension = '.php');
+
+    /**
      * Get a rendered view as a string
      *
      * @param string $view filename of view
